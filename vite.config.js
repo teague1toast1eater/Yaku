2706ec24a653216703c1afd431b3e03791c17855
Teague Beauchamp <a02376169@usu.edu>
2026-04-22 21:44:37 -0600

    another gotcha caught

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+export default {
+    base: '/root/'
+}
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
